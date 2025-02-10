158e69b5a817730cf068337c86274af3e0917bf4
Michel Nicholas <michelnicholas1993@gmail.com>
2025-02-10 10:33:45 -0500

    made changes to my resume project

---
+++ Resume/src/components/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from "react";
+// import { Theme } from "@radix-ui/themes";
+import ProfilePic from "./ProfilePic";
+
+function App() {
+  return (
+    <>
+      {/* <Theme appearance="dark"> */}
+      <ProfilePic />
+      {/* </Theme> */}
+    </>
+  );
+}
+
+export default App;
