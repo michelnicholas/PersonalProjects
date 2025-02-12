b5b80b74127dc4a236f1f901d1c7a7593af5374a
Michel Nicholas <michelnicholas1993@gmail.com>
2025-02-12 11:08:57 -0500

    handled click and hover states its looking good

--- Resume/src/components/WorkHistory.jsx
+++ Resume/src/components/WorkHistory.jsx
@@ -1,3 +1,6 @@
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+
 const workHistory = [
   {
     year: "2023",
@@ -16,20 +19,44 @@ const workHistory = [
   },
 ];
 
-const WorkHistory = () => {
+function WorkHistory() {
+  function handleLinkedInClick() {
+    window.open("https://www.linkedin.com/in/michel-nicholas-65166112a/");
+  }
+
+  function handleGithubClick() {
+    window.open(
+      "https://github.com/michelnicholas?tab=overview&from=2025-02-01&to=2025-02-10"
+    );
+  }
+
   return (
-    <div className="work-history">
-      <h2>Work History</h2>
-      {workHistory.map((job, index) => (
-        <div key={index} className="work-entry">
-          <h3>
-            {job.title} ({job.year})
-          </h3>
-          <p>{job.description}</p>
-        </div>
-      ))}
-    </div>
+    <>
+      <div className="social-links">
+        <LinkedInIcon
+          onClick={handleLinkedInClick}
+          fontSize="large"
+          className="linked-in"
+        />
+        <GitHubIcon
+          className="github"
+          fontSize="large"
+          onClick={handleGithubClick}
+        />
+      </div>
+      <div className="work-history">
+        <h2>Work History</h2>
+        {workHistory.map((job, index) => (
+          <div key={index} className="work-entry">
+            <h3>
+              {job.title} ({job.year})
+            </h3>
+            <p>{job.description}</p>
+          </div>
+        ))}
+      </div>
+    </>
   );
-};
+}
 
 export default WorkHistory;
