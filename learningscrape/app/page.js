da7355d4d221620921bfe0cbe7ac74919276f641
Michel Nicholas <michelnicholas1993@gmail.com>
2025-02-16 21:26:07 -0500

    had to delete

--- learningscrape/app/page.js
+++
@@ -1,9 +0,0 @@
-import RunScraperButton from "./components/RunScraperButton";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gray-900 text-white p-24">
-      <RunScraperButton />
-    </main>
-  );
-}
